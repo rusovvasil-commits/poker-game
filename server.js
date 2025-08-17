8fe41b0cbec4eb911aa3d1d7c2ddaaab42425ec8
rusovvasil-commits <rusov.vasil@gmail.com>
2025-08-17 09:19:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const path = require('path');  // Додати для роботи з шляхами
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));  // Відправка index.html на головну сторінку
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
